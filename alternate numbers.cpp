8ec67ba911e406a0818bee4b07a12057c3f64dbe
hashmahesh <hash1292q@gmail.com>
2024-07-29 13:15:26 +0530

    Update alternate numbers.cpp

--- alternate numbers.cpp
+++ alternate numbers.cpp
@@ -1,3 +1,7 @@
+""" you will be given an array with equal + & - elemnts
+you have to rearrange arr[] in such manner : [+,-,+,-,+,-]
+for e.g : arr[] = [3,1,-2,-5,2,-4] -> [3,-2,1,-5,2,-4] """
+    
 vector<int> alternateNumbers(vector<int>&a) {
     int n = a.size();
     vector<int> ans(n,0);
